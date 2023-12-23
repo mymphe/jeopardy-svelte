d6c6ee73e12fc5919b7148b027a8e76c6b990a29
Azat Serikov <azatsdev@gmail.com>
2023-12-23 16:07:05 +0600

    textarea styles

--- src/lib/File.svelte
+++ src/lib/File.svelte
@@ -21,11 +21,37 @@
 </script>
 
 <form on:submit|preventDefault={submit}>
+  <p id="json-error">{error}</p>
   <textarea
+    id="json"
+    class={error ? "error" : "no-error"}
     placeholder="Вставьте вопросы в формате JSON"
     bind:value={json}
     on:input={validate}
   />
-  <p>{error}</p>
   <button disabled={!!error} type="submit">Далее</button>
 </form>
+
+<style>
+  #json {
+    width: 50%;
+    min-width: 30%;
+    max-width: 70%;
+    height: calc(100vh * 0.8);
+    min-height: calc(100vh * 0.3);
+    max-height: calc(100vh * 0.6);
+    border-radius: 0.5rem;
+  }
+
+  .error {
+    outline: 1px solid red;
+  }
+
+  .no-error {
+    outline: none;
+  }
+
+  #json-error {
+    color: rgb(245, 23, 23);
+  }
+</style>
