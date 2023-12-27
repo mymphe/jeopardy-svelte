6ba323345c69110981c27c21de0e3d69bee1a8de
Azat Serikov <azatsdev@gmail.com>
2023-12-27 13:29:27 +0600

    winner screen

--- src/lib/Winner.svelte
+++ src/lib/Winner.svelte
@@ -33,7 +33,36 @@
   };
 </script>
 
-<Glass>
-  <h2>Yahoo {$winner.name}</h2>
-  <button type="button" on:click={resetGame}>Начать заново</button>
+<Glass title="🎉 Победитель! 🎉">
+  <p class="winner-name">{$winner.name}</p>
+  <button type="button" on:click={resetGame}>Начать заново?</button>
 </Glass>
+
+<style>
+  .winner-name {
+    font-size: 5rem;
+  }
+
+  button {
+    appearance: none;
+    font-size: 1rem;
+    background-color: white;
+    border: 2px solid black;
+    color: black;
+    padding: 0.2rem 2rem;
+    font-family: "Tektur", sans-serif;
+  }
+
+  button:disabled {
+    opacity: 0.3;
+  }
+
+  button:hover:enabled {
+    box-shadow: 4px 4px black;
+  }
+
+  button:active:enabled {
+    box-shadow: 2px 2px black;
+    transform: translate(2px, 2px);
+  }
+</style>
